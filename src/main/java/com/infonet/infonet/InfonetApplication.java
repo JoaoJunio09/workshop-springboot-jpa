30783c0c9949255bd3d73561ca0791166f4f38e9
Joao Junio <joaojunio818@gmail.com>
2024-10-22 21:31:20 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/infonet/infonet/InfonetApplication.java
@@ -0,0 +1,13 @@
+package com.infonet.infonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infonet/infonet/InfonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infonet.infonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
